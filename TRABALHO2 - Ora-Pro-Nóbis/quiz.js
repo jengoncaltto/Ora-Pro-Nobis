e4a0e82172bf696275b611a7306d71c5262c298e
JENIFFER <JENIFFER@DESKTOP-N7BAOQ1>
2023-07-12 18:21:32 -0300

    quiz js

---
+++ TRABALHO2 - Ora-Pro-Nóbis/quiz.js
@@ -0,0 +1,8 @@
+function mostrarPergunta() {
+    var linhas = 0;
+    for( var i = linhas; i < 5; i++) {
+        Math.random()
+        document.getElementById('perguntas').innerHTML += i + '<br>'
+
+    }
+} 
